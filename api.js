4f5919b2516762d36c4f1f41e6356d122f4b8239
Ana Carol <progamacaocarol2021@gmail.com>
2022-12-02 00:15:25 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,47 @@
+const novo = document.getElementById("novo");
+novo.addEventListener('click', () => {
+
+nome()
+});
+
+async function nome  () {
+const iput = document.getElementById("melhor").value;
+const chave = "gyleqlivnOuQzwYqjJ4uQj8MHDkAxANugzxSemyr"
+const name = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${chave}&date=${iput}`);
+const result = await name.json();
+var informacoes = result;
+//console.log(result)
+//var em = document.getElementById("oie");
+//var tela = "aqui esta a data " + result;
+//em.innerHTML = tela;
+console.log(informacoes);
+pegandoImagem (informacoes)
+}
+
+function pegandoImagem (informacoes){
+document.querySelector('.container').innerHTML = informacoes.explanation;
+document.querySelector('.container').innerHTML += `<img src ="${informacoes.url}"  >`
+}
+
+
+
+
+
+
+
+
+
+
+
+//const novo = document.getElementById("novo");
+//const clicar = novo.addEventListener("click", async () => {
+//const id = document.getElementById("melhor").value;
+//const name = await fetch('https://api.nasa.gov/planetary/apod?api_key=gyleqlivnOuQzwYqjJ4uQj8MHDkAxANugzxSemyr');
+//const result = await name.json();
+//const user = new User(result)
+//console.log(clicar)
+//console.log(result)
+//});
+
+
+
---
+++ models/models.js
@@ -0,0 +1,10 @@
+//class User{
+  // c//onstructor(pokemon){
+     //   this.name= pokemon.name;
+      //  this.url= pokemon.url
+      //
+    ////    var bora =  pokemon.name ;
+       // melhor.innerHTML = bora;
+
+      
+//}}
